0f35f75238b93e99bd96134d8032fcea9b09dbad
Adil Labiad <adillabiad@gmail.com>
2023-10-20 18:33:00 -0400

    update k8s  example dag

--- dags/example-dag-k8s.py
+++ dags/example-dag-k8s.py
@@ -1,16 +1,12 @@
-import os
 import datetime
 
-from airflow import configuration
-from airflow.models import DAG, Variable
-from airflow.hooks.base_hook import BaseHook
-from airflow.utils.task_group import TaskGroup
+from airflow.models import DAG
 from plugins.operators.custom_kubernetes_operator import CustomKubernetesPodOperator
 
 
 args = {
     'owner': 'airflow',
-    'start_date': datetime.datetime(2017, 9, 12),
+    'start_date': datetime.datetime(2023, 9, 12),
     'email_on_failure': True,
     'retries': 1,
     'retry_delay': datetime.timedelta(minutes=60)
@@ -21,7 +17,6 @@ dag = DAG(
         dag_id='example_dag_k8s', 
         default_args=args,
         schedule_interval='0 0 * * 0',
-        user_defined_macros={'get_connection': BaseHook.get_connection},
         catchup=False,
         tags=['k8s', 'custom_k8s_pod_operator']
     )
@@ -30,7 +25,7 @@ with dag:
 
     hello_world = CustomKubernetesPodOperator(
                     dag=dag,
-                    image="hello-world:latest",
+                    image="uqam/hello-world:1.0.0",
                     name='hello-world'
                 )
 
