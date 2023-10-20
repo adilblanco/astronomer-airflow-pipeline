cdfae2f87de863935eb44e3f8e5436aa5c14926d
Adil Labiad <adillabiad@gmail.com>
2023-10-20 13:18:31 -0400

    update s3 env variables names

--- plugins/env/s3.py
+++ plugins/env/s3.py
@@ -1,7 +1,7 @@
 def get_s3_env_vars(conn_id):
     return {
-        "S3_BUCKET_NAME": f"{{{{ get_connection('{conn_id}').schema }}}}",
-        "S3_ENDPOINT_URL": f"{{{{ get_connection('{conn_id}').host }}}}",  
+        "S3_BUCKET": f"{{{{ get_connection('{conn_id}').schema }}}}",
+        "S3_ENDPOINT": f"{{{{ get_connection('{conn_id}').host }}}}",  
         "S3_ACCESS_KEY": f"{{{{ get_connection('{conn_id}').login }}}}",
         "S3_SECRET_KEY": f"{{{{ get_connection('{conn_id}').password }}}}"
     }
